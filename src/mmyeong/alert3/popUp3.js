7704813085bdaaf3f5609654bf0c7e97bd88d11c
myeongjunkim <ilikerunning@naver.com>
2022-06-29 03:40:33 +0900

    feat: alert3(요청 보류)

--- src/mmyeong/alert3/popUp3.js
+++ src/mmyeong/alert3/popUp3.js
@@ -1,6 +1,6 @@
 
 function buttonEffect() {
-    const modalBg = document.querySelector('.popUp2');
+    const modalBg = document.querySelector('.popUp3');
     modalBg.classList.toggle('show');
     if (modalBg.classList.contains('show')) {
         document.querySelector("body").style.overflow = 'hidden';
@@ -9,7 +9,7 @@ function buttonEffect() {
 
 
 function modalEffect(e) {
-    const modalBg = document.querySelector('.popUp2');
+    const modalBg = document.querySelector('.popUp3');
     if (e.target == modalBg){
         modalBg.classList.toggle('show');
         if (modalBg.classList.contains('show')) {
