7bd13a8645619c7024754ca967cf82ee99888b3f
myeongjunkim <ilikerunning@naver.com>
2022-08-17 10:30:29 +0900

    feat: nav.js

--- src/may-i/js/dataFetchEx.js
+++ src/may-i/js/dataFetchEx.js
@@ -1,15 +1,17 @@
 
 window.onload = () => {
 
-    fetch("http://127.0.0.1:8000/profile/get-apply-request-all-for-reporter/", {
+    console.log('data.data'); 
+
+    fetch("http://may-i-server.o-r.kr:8000/profile/get-apply-request-all-for-reporter/", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
-          "Authorization": "20f832b6c987d545cb53137e386706bee776d08f",
+          "Authorization": "5a6fd5a0f759438916a2739a34940abc372c31fb",
         }
     }).then((response) => response.json()).then((data) => {
 
-        console.log(data.data) 
+        console.log(data.data); 
         const interviewWrap = document.querySelector('#interviewWrap')
         for(div of data.data){
             const interviewDiv = `<a class="flex p-3 justify-around text-base text-black w-full space-x-10" href="/interview/get-interview/${div.id}}">
