0933042b8c1ad349b20c63bd56fe2d198d7ccb49
nnnayy <kny5439@cau.ac.kr>
2022-08-18 03:24:47 +0900

    fix: nav ui, interview-list ui and proposal-creat ui

--- src/may-i/js/nav.js
+++ src/may-i/js/nav.js
@@ -1,39 +1,39 @@
-
-window.addEventListener('load', () => {
-
-    const nav = 
-    `<nav class="max-w-[1000px] relative overflow-hidden flex mx-8 md:px-20 md:mx-auto justify-between bg-white">
-        <link href="#">
-            <Image src="./img/landing_page1_MayI_logo.svg" alt="logo" width={100} height={80} />
+window.addEventListener("load", () => {
+  const nav = `<nav class="max-w-[1000px] relative overflow-hidden flex mx-8 md:px-20 md:mx-auto justify-between p-5 bg-white">
+        <link href="/">
+        <Image
+            src="/src/may-i/img/landing_page1_MayI_logo.svg"
+            alt="logo"
+            width={100}
+            height={80}
+        />
         </link>
-    <div class="flex items-center ml-auto">
-        <link href=‘#’>
+        <div class="flex items-center ml-auto">
+    <link href=‘#’>
             <a class="hidden md:flex text-sm md:text-lg ml-[0.6rem] md:mr-10 hover:text-indigo-500 font-bold">
                 Expert
             </a>
-        </link>
+            </link>
         <div class="flex items-center ml-auto">
-            <link href=‘#’>
+    <link href=‘#’>
             <a class="hidden md:flex text-sm md:text-lg ml-[0.6rem] md:mr-10 hover:text-indigo-500 font-bold">
                 Reporter
             </a>
             </link>
-            <div class="flex items-center ml-auto">
-                <link href=‘#’>
-                <a class="hidden md:flex text-sm md:text-lg ml-[0.6rem] md:mr-10 hover:text-indigo-500 font-bold">
-                    Q&A
-                </a>
-                </link>
-                <link href="#">
-                <a
-                    class="font-heading text-white font-bold inline-flex items-center justify-center px-[1rem] text-sm md:text-lg py-2 md:px-6 rounded-full transform duration-200 bg-indigo-300 hover:bg-indigo-500">
-                    Sign Up/In
-                </a>
-                </link>
-            </div>
-        </nav>
-    `
+        <div class="flex items-center ml-auto">
+    <link href=‘#’>
+            <a class="hidden md:flex text-sm md:text-lg ml-[0.6rem] md:mr-10 hover:text-indigo-500 font-bold">
+                Q&A
+            </a>
+            </link>
+        <link href="/login">
+            <a class="font-heading text-white font-bold inline-flex items-center justify-center px-[1rem] text-sm md:text-lg py-2 md:px-6 rounded-full transform duration-200 bg-indigo-300 hover:bg-indigo-500">
+            Sign Up/In
+            </a>
+        </link>
+        </div>
+    </nav>
+    `;
 
-    document.querySelector('#navDiv').innerHTML = nav;
-
-})
+  document.querySelector("#navDiv").innerHTML = nav;
+});
