63d2f2262aaebdf394cea548b72daa9b9afce1f5
sueun lee <1015_s@naver.com>
2022-08-19 07:36:45 +0900

    fixed

--- src/may-i/js/proposal-create.js
+++ src/may-i/js/proposal-create.js
@@ -3,28 +3,14 @@ const baseUrl = "http://may-i-server.o-r.kr:8000"
 window.onload = () => {
   const createData = JSON.parse(localStorage.getItem('data'))
   //  여기서 local storage 에 저장한 받는이 전문가 id 값 가져오기
-  // let expertName = "김명명";
-  // let experUserId = 1;
-  // let interviewId = String(2);
-  let expertName = createData["expert_name"]
-  let experUserId = createData["expert_id"]
-  let interviewId = String(createData["id"]);
-  
-  // experUserId = localStorage.getItem("experUserId");
 
-  
-  // experUserId = localStorage.getItem("experUserId");
+  let expertName = createData["expert_name"]
+
+  // 포스트 버튼 달 때
+  let experUserId = createData["expert_id"]
   
 
   document.querySelector('#expert_name').innerHTML += `<p class="w-full text-base font-normal outline-none text-black py-2 px-4">${expertName}</p>`
-
-  const formActionUrl = "http://127.0.0.1:8000/interview/update-interview/" + interviewId
-  
-  // submit 할경우 인터뷰 업데이트
-  const updateInterviewForm = document.querySelector('#updateInterviewForm');
-  updateInterviewForm.setAttribute('action',formActionUrl);
-
-  // send 할 경우 expertUserId 이어붙이고 fetch
 
 }
 
@@ -38,7 +24,8 @@ const changeFileName = (obj) => {
 const formFetching = () => {
   const updateInterviewForm = document.updateInterviewForm;
   
-  let interviewId = String(2);
+  let interviewId = String(createData["id"]);
+
   // experUserId = localStorage.getItem("experUserId");
   
   const formActionUrl = `${baseUrl}/interview/update-interview/` + interviewId
